07758063dad2031003386e1dbf92874470ed9af0
James Youngblood <james@youngbloods.org>
2018-09-05 21:56:42 -0600

    Styling welcome screen

--- src/components/app.js
+++ src/components/app.js
@@ -3,16 +3,16 @@ import React, { Component } from 'react';
 import style from '../style.js';
 
 import Background from './background';
+import Header from './header';
 
 class App extends Component {
   render() {
     return (
       <div style={style.appWrapper} className='app-wrapper'>
-        <Background>
-          <main style={style.main}>
-            testing
-          </main>
-        </Background>
+        <Background/>
+        <main style={style.main}>
+          <Header/>
+        </main>
       </div>
     );
   }
--- src/style.js
+++ src/style.js
@@ -1,19 +1,38 @@
 const style = {
-  appWrapper: {
+  main: {
+    width: '100vw',
+    height: '100vh',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+  },
+  backgroundImage: {
+    width: '102vw',
+    height: '102vh',
+    transform: 'translate(-1%, -1%)',
+    objectFit: 'cover',
+    transition: 'filter 0.5s'
+  },
+  backgroundImageBlur: {
+    width: '102vw',
+    height: '102vh',
+    transform: 'translate(-1%, -1%)',
+    objectFit: 'cover',
+    filter: 'blur(6px)'
+  },
+  welcomeWrapper: {
+    marginTop: '10vh',
     display: 'flex',
     justifyContent: 'center'
   },
-  main: {
-    background: 'rgba(0,0,0,0.4)',
-    width: '60em',
-    height: '40em'
+  welcomeCrest: {
+    transform: 'translate(2%, 0)',
+    transition: 'all 0.5s'
   },
-  backgroundImage: {
-    width: '100vw',
-    height: '100vh',
-    objectFit: 'cover',
-    filter: 'blur(6px)',
-    zIndex: '-10'
+  welcomeh1: {
+    textShadow: '0 0 10px rgba(250, 250, 240, 0.8)',
+    textAlign: 'center'
   }
 }
 
